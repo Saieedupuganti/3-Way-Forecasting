dd936b98274c3cb0befe4d26d09c3bb371b7f817
sree1223 <sreeharsha120203@gmail.com>
2024-07-05 16:24:11 +0530

    redirect login

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,11 @@ import {
   IconButton,
   Card,
 } from "@material-tailwind/react";
- 
+import { Bars3CenterLeftIcon } from "@heroicons/react/24/outline";
+
+
+
+
 export default function Home() {
   const [openNav, setOpenNav] = React.useState(false);
  
@@ -104,113 +108,48 @@ export default function Home() {
   );
  
   return (
-    <div className="max-h-[768px] w-[calc(100%)] overflow-scroll">
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+    <div className="h-full flex-grow w-[100%] overflow-scroll">
+      <Navbar className="sticky top-0 h-max max-w-full rounded-none shadow-lg px-4 py-2 lg:px-8 lg:py-2 bg-gray-900">
         <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 cursor-pointer py-1.5 font-medium"
-          >
-            Material Tailwind
-          </Typography>
+        <Bars3CenterLeftIcon class="h-6 w-6 text-white mr-4" />
+
+            <img src="logo1.png" alt="C Suite Navigator Image" className="mr-auto w-14 py-0 inline-block" />
+            <h2 className='w-full mx-auto text-center text-2xl'>C Suite Navigator</h2>
           <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
+            {/* <div className="mr-4 hidden lg:block">{navList}</div> */}
+            
             <div className="flex items-center gap-x-1">
-              <Button
+              {/* <Button
                 variant="text"
                 size="sm"
                 className="hidden lg:inline-block"
               >
                 <span>Log In</span>
-              </Button>
+              </Button> */}
               <Button
                 variant="gradient"
                 size="sm"
-                className="hidden lg:inline-block"
+                className="hidden lg:inline-block min-w-24 py-3"
                 onClick={handleSignOut}
               >
                 <span>Sign Out</span>
               </Button>
             </div>
-            <IconButton
-              variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
-            >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
           </div>
         </div>
         <MobileNav open={openNav}>
-          {navList}
+          {/* {navList} */}
           <div className="flex items-center gap-x-1">
-            <Button fullWidth variant="text" size="sm" className="">
+            {/* <Button fullWidth variant="text" size="sm" className="">
               <span>Log In</span>
-            </Button>
+            </Button> */}
             <Button fullWidth variant="gradient" size="sm" className="">
               <span>Sign in</span>
             </Button>
           </div>
         </MobileNav>
       </Navbar>
-      <div className="mx-auto max-w-screen-md py-12">
-        <Card className="mb-12 overflow-hidden">
-          <img
-            alt="nature"
-            className="h-[32rem] w-full object-cover object-center"
-            src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
-          />
-        </Card>
-        <Typography variant="h2" color="blue-gray" className="mb-2">
-          What is Material Tailwind
-        </Typography>
-        <Typography color="gray" className="font-normal">
-          Can you help me out? you will get a lot of free exposure doing this
-          can my website be in english?. There is too much white space do less
-          with more, so that will be a conversation piece can you rework to make
-          the pizza look more delicious other agencies charge much lesser can
-          you make the blue bluer?. I think we need to start from scratch can my
-          website be in english?, yet make it sexy i&apos;ll pay you in a week
-          we don&apos;t need to pay upfront i hope you understand can you make
-          it stand out more?. Make the font bigger can you help me out? you will
-          get a lot of free exposure doing this that&apos;s going to be a chunk
-          of change other agencies charge much lesser. Are you busy this
-          weekend? I have a new project with a tight deadline that&apos;s going
-          to be a chunk of change. There are more projects lined up charge extra
-          the next time.
-        </Typography>
-      </div>
+      
     </div>
   );
 }
